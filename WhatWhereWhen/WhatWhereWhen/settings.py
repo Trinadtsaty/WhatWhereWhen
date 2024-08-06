e2453d325108c06fc8951412c4b9301759155227
Trinadtsatyy <ni9htwatch@yandex.ru>
2024-08-06 13:24:05 +0300

    1

--- WhatWhereWhen/WhatWhereWhen/settings.py
+++ WhatWhereWhen/WhatWhereWhen/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'WhatWhereWhen.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'DB/db.sqlite3',
+    },
+    "pack": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'DB/packs.sqlite3',
     }
 }
 
