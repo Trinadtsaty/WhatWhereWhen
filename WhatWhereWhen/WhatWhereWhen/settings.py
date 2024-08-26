d1b0fa6316da10104537aec2de1decb3cbf7abf6
Trinadtsatyy <ni9htwatch@yandex.ru>
2024-08-26 14:45:48 +0300

    1

--- WhatWhereWhen/WhatWhereWhen/settings.py
+++ WhatWhereWhen/WhatWhereWhen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jva0)u5))%9z6-3f=$+-gnu#@^&2+6o06z=kqq86+bu9^u$i2'
+SECRET_KEY = 'django-insecure-=4@oy19qa_-f&enqy)9vp7h&f=h@nusx68tu_*@m7n$+v1^&63'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +76,7 @@ WSGI_APPLICATION = 'WhatWhereWhen.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'DB/db.sqlite3',
-    },
-    "pack": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'DB/packs.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
