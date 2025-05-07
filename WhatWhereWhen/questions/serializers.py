e2a2cf354170f08d5296642e997ae2b1e9f92072
Trinadtsatyy <91557208+Trinadtsaty@users.noreply.github.com>
2025-05-07 12:07:29 +0300

    Работаю с API готовлюсь отправлять POST и GET запросы

---
+++ WhatWhereWhen/questions/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Titles
+
+
+
+
+class TitlesModel:
+    def __init__(self, ):
--- WhatWhereWhen/questions/static/questions/js/test.js
+++ WhatWhereWhen/questions/static/questions/js/test.js
@@ -1,11 +1,48 @@
 const URL_server = 'http://127.0.0.1:8000'
+console.log(URL_server + '/questions/api/v1/')
+
+
+fetch(URL_server + '/questions/api/v1', {
+    method: 'GET', // Указываем метод запроса
+})
+    .then((response) => {
+        // Проверяем, успешен ли ответ
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // Преобразуем ответ в JSON
+    })
+    .then((data) => {
+        console.log(data); // Обработка полученных данных
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
 
 
 const data = {
-    "estimation" : 2,
+    'title' : "Оценка",
+    'description': 3,
 }
 
-
-fetch(URL_server + '/questions/api/v1/')
-
+fetch(URL_server + '/questions/api/v1', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json', // Указываем тип контента
+    },
+    body: JSON.stringify(data),
+})
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
 
