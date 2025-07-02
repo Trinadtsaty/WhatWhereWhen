eed6c18133dff5f87bd30c08f892bf5b58b311e6
Trinadtsatyy <ni9htwatch@yandex.ru>
2025-07-02 20:31:14 +0300

    Подготовка к пушу

--- WhatWhereWhen/WhatWhereWhen/asgi.py
+++ WhatWhereWhen/WhatWhereWhen/asgi.py
@@ -43,7 +43,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatWhereWhen.settings')
+import django
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
