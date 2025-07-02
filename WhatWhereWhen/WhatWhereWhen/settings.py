ad41a57b60dd49077a504491a93037cfc394369d
Trinadtsatyy <ni9htwatch@yandex.ru>
2025-07-02 16:19:50 +0300

    Подготовка к пушу

--- WhatWhereWhen/WhatWhereWhen/settings.py
+++ WhatWhereWhen/WhatWhereWhen/settings.py
@@ -159,10 +159,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # "/var/www/static/",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     # "/var/www/static/",
+# ]
 
 AUTH_USER_MODEL = 'registration.Users'
 
