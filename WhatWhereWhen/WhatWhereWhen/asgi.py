9b553105387e38fa7f50d5aa3ea3b8ed53bee4f8
Trinadtsatyy <ni9htwatch@yandex.ru>
2025-07-02 20:09:24 +0300

    Подготовка к пушу

--- WhatWhereWhen/WhatWhereWhen/asgi.py
+++ WhatWhereWhen/WhatWhereWhen/asgi.py
@@ -7,23 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-# from main.routing import websocket_urlpatterns  # импортируем маршруты
-import main.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatWhereWhen.settings')
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main.routing.websocket_urlpatterns
-        )
-    ),
-})
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# # from main.routing import websocket_urlpatterns  # импортируем маршруты
+# import main.routing
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatWhereWhen.settings')
+#
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             main.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
 
 # from channels.auth import AuthMiddlewareStack
 # from channels.routing import ProtocolTypeRouter, URLRouter
@@ -40,3 +40,21 @@ application = ProtocolTypeRouter({
 #         )
 #     ),
 # })
+
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatWhereWhen.settings')
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import main.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
+    ),
+})
